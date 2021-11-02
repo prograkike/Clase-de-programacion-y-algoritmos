c922d8a5bbf8dea9844db158b7a10c94271c7409
Olivia Barron <olivia.barron@udem.edu>
2021-11-02 14:53:27 -0600

    valida a con arreglo de caracteres

--- Raices.cpp
+++ Raices.cpp
@@ -1,10 +1,11 @@
 # include <iostream>
-# include <math.h>
+# include <cmath>
+# include <cstdlib>
 
 using namespace std;
 
 // Ejemplo de uso de la libreria math
-// Este programa calcula las raíces de una ecuación cuadráica
+// Este programa calcula las raíces de una ecuación cuadrática
 // de la forma ax2 + bx + c = 0
 // utilizando la fórmula general
 
@@ -15,19 +16,30 @@ int main()
  // y para las raíces (x1, x2)
  float a, b, c, x1, x2;
 
+ // Agregamos el string para validar que no tenga comportamiento
+ // extraño si el usuario se equivoca
+ char valida_a[100];
+
 // Pedimos los valores de los coeficientes al usuario
-cout << "Dame el valor del coeficiente cuadrático ";
-cin >> a;
+
+do
+{
+    cout << "Dame el valor del coeficiente cuadrático ";
+    cin >> valida_a;
+    a = atof(valida_a);
+} while (a==0); // a no debe ser 0
+
 cout << "Dame el valor del coeficiente lineal ";
 cin >> b;
+
 cout << "Dame el valor del coeficiente constante ";
 cin >> c;
 
-// Esta versión no valida las entradas
+// Esta versión no valida el determinante
 // calculamos la primera raíz
-x1 = (-b + sqrt(pow(b,2.0)-4*a*c))/2*a;
+x1 = (-b + sqrt(pow(b,2.0)-4*a*c))/(2*a);
 // y la segunda raiz
-x2 = (-b - sqrt(pow(b,2.0)-4*a*c))/2*a;
+x2 = (-b - sqrt(pow(b,2.0)-4*a*c))/(2*a);
 
 //Desplegamos ls resultados
 cout << "La primera raíz es "<< x1 <<endl;
